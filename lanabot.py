597b0462e1a5972b439148acfd17c49013a8ddf1
w1r4 <wirayudhia@gmail.com>
2019-12-17 11:51:28 +0700

    Update lanabot.py

--- lanabot.py
+++ lanabot.py
@@ -119,12 +119,12 @@ def getWeather(place):
     
 def getAdzan(place):
     ts = calendar.timegm(time.gmtime())
-        lat, lon = place["latitude"], place["longitude"]
-        url = URL_ADZAN +  ts + "?" + "&lat=%f&lon=%f&cnt=1" % (lat, lon)
-        logger.info("Requesting adzan time : " + url)
-        js = makeRequest(url)
-        logger.debug(js)
-        return getAdzan(js)
+    lat, lon = place["latitude"], place["longitude"]
+    url = URL_ADZAN +  ts + "?" + "&lat=%f&lon=%f&cnt=1" % (lat, lon)
+    logger.info("Requesting adzan time : " + url)
+    js = makeRequest(url)
+    logger.debug(js)
+    return getAdzan(js)
 
 
 # Send URL-encoded message to chat id
